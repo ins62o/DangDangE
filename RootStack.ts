792447f4e5d0ae5cf0453e0563f1b7a352f0460d
inseongei <twin2688@naver.com>
2025-01-27 19:43:06 +0900

    화면 네비게이션 타입 정의 &  Stack 경로 추가

--- RootStack.ts
+++ RootStack.ts
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Splash from "./pages/Splash";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Welcome from "./pages/Welcome";
+import BloodType from "./pages/BloodType";
 
 export const RootStack = createNativeStackNavigator({
   screens: {
@@ -21,6 +23,19 @@ export const RootStack = createNativeStackNavigator({
 
     SignUp: {
       screen: SignUp,
+      options: {
+        headerShown: false,
+      },
+    },
+
+    Welcome: {
+      screen: Welcome,
+      options: {
+        headerShown: false,
+      },
+    },
+    BloodType: {
+      screen: BloodType,
       options: {
         headerShown: false,
       },
---
+++ types/stackType.ts
@@ -0,0 +1,7 @@
+export type StackParamList = {
+  Splash: undefined;
+  Login: undefined;
+  SignUp: undefined;
+  Welcome: undefined;
+  BloodType: undefined;
+};
