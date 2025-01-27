47f47fe27346c3554db1ab9419e56a21ae9a301e
inseongei <twin2688@naver.com>
2025-01-27 19:44:21 +0900

    당뇨 종류 이미지 & 당뇨 데이터 Mock 생성

---
+++ InitialData.js
@@ -0,0 +1,22 @@
+export const BloodInfo = {
+  oneBlood: {
+    info: "우리나라 당뇨병의 2% 미만을 차지하며 30세 전에 진단되는 경우가 많지만 성인에서도 나타날 수 있습니다.",
+    offimage: require("./assets/image/disoneBlood.png"),
+    onimage: require("./assets/image/oneBlood.png"),
+  },
+  twoBlood: {
+    info: "한국인 당뇨병의 대부분이 2형 당뇨이며, 보통 40세 이상에서 발생하지만 최근에는 30세 이하의 젊은 환자가 늘고 있습니다.",
+    offimage: require("./assets/image/distwoBlood.png"),
+    onimage: require("./assets/image/twoBlood.png"),
+  },
+  threeBlood: {
+    info: "유전자 결함, 유전질환, 약물, 감염에 의해 발생하는 당뇨병입니다.",
+    offimage: require("./assets/image/disthreeBlood.png"),
+    onimage: require("./assets/image/threeBlood.png"),
+  },
+  fourBlood: {
+    info: "임신 중에 발견된 당뇨병을 지칭합니다. 임신 기간은 물론, 출산 후에도 장기적으로 당뇨병 예방 조치를 취해야 합니다.",
+    offimage: require("./assets/image/disfourBlood.png"),
+    onimage: require("./assets/image/fourBlood.png"),
+  },
+};
