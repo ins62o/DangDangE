89c0fbdbde7d82c1aa5285c693d05de3be1bb755
inseongei <twin2688@naver.com>
2025-01-27 14:30:02 +0900

    패키지 설치 & 공동 디자인 구성 & 로고 파일 수정

--- App.tsx
+++ App.tsx
@@ -1,14 +1,4 @@
-import {
-  createStaticNavigation,
-  NavigationContainer,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Splash from "./pages/Splash";
-import { StyleSheet } from "react-native";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import { colors } from "./common";
-import AntDesign from "@expo/vector-icons/AntDesign";
+import { createStaticNavigation } from "@react-navigation/native";
 import { RootStack } from "./RootStack";
 
 export default function App() {
@@ -16,9 +6,3 @@ export default function App() {
 
   return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  headerStyle: {
-    backgroundColor: colors.Main,
-  },
-});
--- common.tsx
+++ common.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
+
+export const colors = {
+  Main: "#2C6975",
+  Sub1: "#68B2A0",
+  Sub2: "#E0ECDE",
+  Point: "#FFDA40",
+  Error: "#FF5100",
+  Good: "#191554",
+  DimGrey: "#646464",
+  Nobel: "#979797",
+  WhiteSmoke: "#F6F6F6",
+};
+
+export const fonts = {
+  Headline: Platform.OS === "ios" ? 26 : 18,
+  Subline: Platform.OS === "ios" ? 20 : 14,
+  body: Platform.OS === "ios" ? 14 : 12,
+  description: Platform.OS === "ios" ? 12 : 10,
+};
 
 export const CommonStyle = StyleSheet.create({
   input: {
-    width: "100%",
-    height: 40,
-    borderWidth: 1,
-    borderColor: "#ccc",
+    backgroundColor: "#fff",
+    width: "80%",
+    marginBottom: 25,
+    padding: 10,
     borderRadius: 8,
-    marginVertical: 10,
-    paddingHorizontal: 10,
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.2,
+    shadowRadius: 20,
+    elevation: 5,
   },
 
   button: {
@@ -16,19 +38,7 @@ export const CommonStyle = StyleSheet.create({
     height: 40,
     borderRadius: 8,
     justifyContent: "center",
+    backgroundColor: colors.Main,
+    alignItems: "center",
   },
 });
-
-export const colors = {
-  primary: "#2C6975",
-  secondary: "#68B2A0",
-  third: "#CDE0C9",
-  sub: "#E0ECDE",
-};
-
-export const fonts = {
-  XLarge: 32,
-  Large: 24,
-  medium: 16,
-  small: 12,
-};
