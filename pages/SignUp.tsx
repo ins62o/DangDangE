0970fe8c0a963199112e8cd88832f917288f59e5
inseongei <twin2688@naver.com>
2025-01-27 14:30:55 +0900

    회원가입 UI 구현 ( 키보드 가림 문제 해결 )

--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,93 +1,68 @@
+import React from "react";
 import {
-  View,
-  StyleSheet,
-  Image,
-  TextInput,
-  Text,
-  TouchableOpacity,
-  Platform,
-  Pressable,
   KeyboardAvoidingView,
+  Platform,
   ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
-import { CommonStyle, colors, fonts } from "../common";
-const Icon = require("../assets/image/LogoIcon.png");
+import { colors, CommonStyle, fonts } from "../common";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type StackParamList = {
+  Splash: undefined;
+  Login: undefined;
+  SignUp: undefined;
+};
 
 export default function SignUp() {
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={Icon} />
+      <View style={styles.titleContainer}>
+        <AntDesign
+          name="arrowleft"
+          size={24}
+          color="#fff"
+          style={styles.arrow}
+          onPress={() => navigation.goBack()}
+        />
+        <Text style={texts.title}>회원가입</Text>
       </View>
+      <KeyboardAvoidingView
+        style={styles.signUpContainer}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+          <View style={CommonStyle.input}>
+            <Text style={texts.body}>닉네임</Text>
+            <TextInput style={styles.input} placeholder="당당이" />
+          </View>
 
-      <KeyboardAvoidingView style={BoxStyles.loginBox} behavior="padding">
-        <ScrollView style={BoxStyles.scrollBox}>
-          <View style={BoxStyles.inputBox}>
-            <View style={BoxStyles.marginBox}>
-              <Text style={TextStyles.inputInfo}>닉네임</Text>
-              <View style={BoxStyles.NickBox}>
-                <View style={styles.inputLength}>
-                  <TextInput
-                    style={CommonStyle.input}
-                    autoCapitalize={"none"}
-                  />
-                </View>
-                <View style={styles.buttonLength}>
-                  <Pressable
-                    style={[
-                      CommonStyle.button,
-                      { backgroundColor: colors.secondary },
-                    ]}
-                  >
-                    <Text style={TextStyles.NickText}>중복체크</Text>
-                  </Pressable>
-                </View>
-              </View>
-            </View>
+          <View style={CommonStyle.input}>
+            <Text style={texts.body}>아이디</Text>
+            <TextInput style={styles.input} placeholder="user@email.com" />
+          </View>
 
-            <View style={BoxStyles.marginBox}>
-              <Text style={TextStyles.inputInfo}>아이디</Text>
-              <TextInput
-                style={CommonStyle.input}
-                autoCapitalize={"none"}
-                autoCorrect={false}
-                spellCheck={false}
-                textContentType="none"
-              />
-            </View>
+          <View style={CommonStyle.input}>
+            <Text style={texts.body}>비밀번호</Text>
+            <TextInput style={styles.input} placeholder="******" />
+          </View>
 
-            <View style={BoxStyles.marginBox}>
-              <Text style={TextStyles.inputInfo}>비밀번호</Text>
-              <TextInput
-                style={CommonStyle.input}
-                secureTextEntry
-                autoCorrect={false}
-                spellCheck={false}
-                textContentType="none"
-              />
-            </View>
+          <View style={CommonStyle.input}>
+            <Text style={texts.body}>비밀번호 확인</Text>
+            <TextInput style={styles.input} placeholder="******" />
+          </View>
 
-            <View style={BoxStyles.marginBox}>
-              <Text style={TextStyles.inputInfo}>비밀번호 확인</Text>
-              <TextInput
-                style={CommonStyle.input}
-                secureTextEntry
-                autoCorrect={false}
-                spellCheck={false}
-                textContentType="none"
-              />
-            </View>
-
-            <View style={BoxStyles.marginBox}>
-              <TouchableOpacity
-                style={[
-                  CommonStyle.button,
-                  { backgroundColor: colors.primary },
-                ]}
-              >
-                <Text style={TextStyles.signUpText}>회원가입</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={CommonStyle.button}>
+              <Text style={texts.buttonText}>회원가입</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
@@ -98,68 +73,49 @@ export default function SignUp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.Main,
+    paddingTop: 55,
   },
-  header: {
-    flex: 0.3,
+  titleContainer: {
+    flex: 0.12,
     justifyContent: "center",
     alignItems: "center",
   },
-  inputLength: {
-    width: "70%",
-  },
-  buttonLength: {
-    width: "25%",
-    marginLeft: "5%",
-  },
-});
-
-const TextStyles = StyleSheet.create({
-  inputInfo: {
-    alignSelf: "flex-start",
-    fontSize: Platform.OS === "android" ? fonts.small : fonts.medium,
-  },
-  signUpText: {
-    color: "#fff",
-    textAlign: "center",
-    fontSize: Platform.OS === "android" ? fonts.small : fonts.medium,
-  },
-  NickText: {
-    color: "#fff",
-    textAlign: "center",
-    fontSize: Platform.OS === "android" ? fonts.small : fonts.medium,
-  },
-});
-
-const BoxStyles = StyleSheet.create({
-  scrollBox: {
-    flex: 1,
-  },
-  loginBox: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: colors.sub,
-    borderStyle: "solid",
-    borderRadius: 20,
+  signUpContainer: {
+    flex: 0.88,
+    backgroundColor: colors.WhiteSmoke,
+    borderRadius: 80,
+    borderTopEndRadius: 0,
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
-    backgroundColor: colors.sub,
-    paddingTop: 10,
+    justifyContent: "center",
+    paddingTop: 50,
   },
-  marginBox: {
-    alignItems: "center",
+  arrow: {
+    position: "absolute",
+    left: 0,
+    paddingLeft: 15,
+  },
+  input: {
+    marginTop: 5,
+    height: 40,
+    fontSize: fonts.body,
+  },
+  buttonContainer: {
     width: "80%",
-    marginTop: 20,
   },
-  TextBox: {
-    marginTop: 60,
+});
+
+const texts = StyleSheet.create({
+  title: {
+    fontSize: fonts.Headline,
+    color: "#fff",
+    fontWeight: "bold",
   },
-  inputBox: {
-    flex: 0.6,
-    alignItems: "center",
+  body: {
+    fontSize: fonts.body,
   },
-  NickBox: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
+  buttonText: {
+    color: "#fff",
   },
 });
