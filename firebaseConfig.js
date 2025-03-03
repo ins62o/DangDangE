038c1b4513bea758e97fbbdb5f4ae247a3cc33e6
inseongei <twin2688@naver.com>
2025-03-03 18:13:59 +0900

    파이어베이스 키값 env 관리

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-tc4ffRBMXwnECpAm00KoXpSsjXEkkEE",
-  authDomain: "dangdange-16cc2.firebaseapp.com",
-  projectId: "dangdange-16cc2",
-  storageBucket: "dangdange-16cc2.firebasestorage.app",
-  messagingSenderId: "560972189472",
-  appId: "1:560972189472:web:1d4d4314b1d4c1830d9145",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
